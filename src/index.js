b719318c238166936362c3b9909aa38e4b1e237d
Ademola Adegbuyi <ooade96@gmail.com>
2017-03-23 21:11:59 +0100

    feat(RR): SOC

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
-import React, { Component, PropTypes } from 'react';
+/**
+ * React Rough - Binds React with Rough.js
+ * A RR Component takes in:
+ * - width <Number>
+ * - height <Number>
+ * - onRender <Function>
+ * - options <Object => data, ...>
+ */
+
+import React from 'react';
 import RoughCanvas from './rough';
 
-class CanvasComponent extends Component {
+class ReactRough extends React.Component {
+  _createCanvas() {
+    this.canvas = document.createElement('canvas');
+    document.body.appendChild(this.canvas);
+  }
+
+  _updateCanvas() {
+    const {
+      width,
+      height,
+      onRender,
+      options: { data, ...rest }
+    } = this.props;
+
+    this.rough = new RoughCanvas(this.canvas, width, height);
+
+    // Iterate over props and apply each prop to our element
+    Object.keys(rest).forEach(prop => this.rough[prop] = rest[prop]);
+
+    let shape;
+
+    // Apply our supplied data to rough
+    // path => String, curve and polygon => Array
+    if (this.name.match(/curve|path|polygon/)) {
+      shape = this.rough[this.name](data);
+    } else {
+      shape = this.rough[this.name](...data);
+    }
+
+    // Apply our Hook
+    onRender && onRender(shape);
+  }
+
+  _deleteCanvas() {
+    document.body.removeChild(this.canvas);
+  }
+
   componentDidMount() {
+    this._createCanvas();
     this._updateCanvas();
   }
 
@@ -10,45 +56,54 @@ class CanvasComponent extends Component {
     this._updateCanvas();
   }
 
-  _updateCanvas() {
-    const {
-      width,
-      height,
-      shape,
-      data,
-      ...props
-    } = this.props;
-
-    const rough = new RoughCanvas(this.refs.roughcanvas, width, height);
-
-    // Iterate over props and apply each prop to our element
-    Object.keys(props).forEach(prop => rough[prop] = props[prop]);
-
-    // Apply our supplied data to rough
-    rough[shape](...data);
+  componentWillUnmount() {
+    this._deleteCanvas();
   }
 
   render() {
-    return <canvas ref='roughcanvas'/> ;
+    return null;
   }
 }
 
-export default class extends React.Component {
-  // Add the PropTypes we care about
-  static propTypes = {
-    width: React.PropTypes.number.isRequired,
-    height: React.PropTypes.number.isRequired,
-    shape: React.PropTypes.string.isRequired,
-    data: React.PropTypes.array.isRequired
-  }
-
-  // Defaultprops to create Canvas
-  static defaultProps = {
-    width: 100,
-    height: 100
-  }
-
-  render() {
-    return <CanvasComponent {...this.props}/>;
-  }
+class Arc extends ReactRough {
+  name = 'arc';
 }
+
+class Circle extends ReactRough {
+  name = 'circle';
+}
+
+class Curve extends ReactRough {
+  name = 'curve';
+}
+
+class Ellipse extends ReactRough {
+  name = 'ellipse';
+}
+
+class Line extends ReactRough {
+  name = 'line';
+}
+
+class Path extends ReactRough {
+  name = 'path';
+}
+
+class Polygon extends ReactRough {
+  name = 'polygon';
+}
+
+class Rectangle extends ReactRough {
+  name = 'rectangle';
+}
+
+export {
+  Arc,
+  Circle,
+  Curve,
+  Ellipse,
+  Line,
+  Path,
+  Polygon,
+  Rectangle
+};
--- test/index.js
+++ test/index.js
@@ -1,7 +1,5 @@
 // import { expect } from 'chai';
 import React from 'react';
 import ReactDOM from 'react-dom/server';
-import ReactRough from '../src';
 
-const data = [10, 10, 200, 200];
 // TODO
