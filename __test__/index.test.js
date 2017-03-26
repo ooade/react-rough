86c51dd2115b3df59c2f9f9ed16ca2ae30e36c52
Ademola Adegbuyi <ooade96@gmail.com>
2017-03-26 23:05:04 +0100

    Better Coverage, export RR

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import toJSON from 'enzyme-to-json';
+import { render } from 'enzyme';
 
 import {
   Arc,
@@ -10,74 +9,128 @@ import {
   Line,
   Path,
   Polygon,
-  Rectangle
+  Rectangle,
+  ReactRough
 } from '../src';
 
 /* Global => expect */
 const options = {
-  fill: '#3ef6ad',
-  data: [200, 100, 200, 180, -Math.PI + (Math.PI / 3), -Math.PI / 2, true]
+  arc: {
+    data: [200, 100, 200, 180, -Math.PI + (Math.PI / 3), -Math.PI / 2, true]
+  },
+  rectangle: {
+    fill: 'red',
+    data: [10, 10, 200, 200]
+  },
+  circle: {
+    data: [80, 120, 50] // centerX, centerY, radius
+  },
+  curve: {
+    data: [[10, 10], [150, 65], [180, 165], [300, 20], [400, 200]]
+  },
+  ellipse: {
+    data: [300, 100, 150, 80] // centerX, centerY, radiusX, radiusY
+  },
+  line: {
+    data: [80, 120, 300, 100] // x1, y1, x2, y2
+  },
+  path: {
+    data: 'M213.1,6.7c-32.4-14.4-73.7,0-88.1,30.6C110.6,4.9,67.5-9.5,36.9,6.7C2.8,22.9-13.4,62.4,13.5,110.9 C33.3,145.1,67.5,170.3,125,217c59.3-46.7,93.5-71.9,111.5-106.1C263.4,64.2,247.2,22.9,213.1,6.7z'
+  }
 };
 
-it ('renders Arc correctly', () => {
-  const wrapper = shallow(
-    <Arc width={120} height={120} options={options}/>
+const increaseWidth = rect => {
+  if (rect.width < 200) {
+    rect.width = rect.width + 10;
+    setTimeout(() => increaseWidth(rect), 100);
+  }
+};
+
+// Fake window.requestAnimationFrame
+window.requestAnimationFrame = () => ({});
+
+it ('renders ReactRough with nested Components', () => {
+  const wrapper = render(
+    <ReactRough width={800} height={700} >
+      <Arc options={options.arc}/>
+      <Rectangle options={options.rectangle}/>
+    </ReactRough>
   );
-  console.log(toJSON(wrapper));
-  expect(toJSON(wrapper)).toMatchSnapshot();
+
+  expect(wrapper).toMatchSnapshot();
+});
+
+it ('renders Arc correctly', () => {
+  const wrapper = render(
+    <Arc width={120} height={120} options={options.arc}/>
+  );
+  expect(wrapper).toMatchSnapshot();
 });
 
 it ('renders Circle correctly', () => {
-  const wrapper = shallow(
-    <Circle width={120} height={120} options={options}/>
+  const wrapper = render(
+    <Circle width={120} height={120} options={options.circle}/>
   );
 
   expect(wrapper).toMatchSnapshot();
 });
 
 it ('renders Curve correctly', () => {
-  const wrapper = shallow(
-    <Curve width={120} height={120} options={options}/>
+  const wrapper = render(
+    <Curve width={120} height={120} options={options.curve}/>
   );
 
   expect(wrapper).toMatchSnapshot();
 });
 
 it ('renders Ellipse correctly', () => {
-  const wrapper = shallow(
-    <Ellipse width={120} height={120} options={options}/>
+  const wrapper = render(
+    <Ellipse width={120} height={120} options={options.ellipse}/>
   );
 
   expect(wrapper).toMatchSnapshot();
 });
 
 it ('renders Line correctly', () => {
-  const wrapper = shallow(
-    <Line width={120} height={120} options={options}/>
+  const wrapper = render(
+    <Line width={120} height={120} options={options.line}/>
   );
 
   expect(wrapper).toMatchSnapshot();
 });
 
 it ('renders Path correctly', () => {
-  const wrapper = shallow(
-    <Path width={120} height={120} options={options}/>
+  const wrapper = render(
+    <Path width={120} height={120} options={options.path}/>
   );
 
   expect(wrapper).toMatchSnapshot();
 });
 
 it ('renders Polygon correctly', () => {
-  const wrapper = shallow(
-    <Polygon width={120} height={120} options={options}/>
+  const wrapper = render(
+    <Polygon width={120} height={120} options={options.rectangle}/>
   );
 
   expect(wrapper).toMatchSnapshot();
 });
 
 it ('renders Rectangle correctly', () => {
-  const wrapper = shallow(
-    <Rectangle width={120} height={120} options={options}/>
+  const wrapper = render(
+    <Rectangle width={120} height={120} options={options.rectangle}/>
+  );
+
+  expect(wrapper).toMatchSnapshot();
+});
+
+it ('renders Rectangle with the onRender Hook correctly', () => {
+  const wrapper = render(
+    <Rectangle
+      width={120}
+      height={120}
+      options={options.rectangle}
+      onRender={increaseWidth}
+    />
   );
 
   expect(wrapper).toMatchSnapshot();
--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 /**
  * React Rough - Binds React with Rough.js
- * A RR Component takes in:
- * - width <Number>
- * - height <Number>
- * - onRender <Function>
- * - options <Object => data, ...>
  */
 
 /* @flow */
@@ -13,15 +8,13 @@ import React from 'react';
 import RoughCanvas from './rough';
 
 type Props = {
-  name: string,
   width: number,
   height: number,
-  'data-rough-id': string,
-  onRender: () => void,
+  children?: Node,
   options: {
-    id: string,
     data: string | Array<number> | Array<Array<number>>
-  }
+  },
+  onRender?: () => void
 }
 
 class RoughComponent extends React.Component {
@@ -29,80 +22,54 @@ class RoughComponent extends React.Component {
 
   name: string;
 
-  canvas: HTMLCanvasElement;
-
   _prepareRough() {
-  let canvas, rough;
+    const { width, height } = this.props;
 
-  // Transverse through child components
-  if (this.props.children) {
-    // Is Canvas Created?
-    // Yea, fetch the canvas (0_0)
-    canvas = [
-      ...document.querySelectorAll('[data-rough-id]')
-    ].find(rough => rough.dataset.roughId === this.props['data-rough-id']);
-
-    if (canvas === undefined) {
-      // Create one ;)
-      canvas = document.createElement('canvas');
-      canvas.dataset.roughId = this.props['data-rough-id'];
-      document.body.appendChild(canvas);
-
+    // Transverse through child components
+    if (this.props.children) {
       // Create RoughCanvas
-      rough = new RoughCanvas(canvas, this.props.width, this.props.height);
-    } else { return; }
+      let rough = new RoughCanvas(this.refs.roughCanvas, width, height);
 
-    React.Children.toArray(this.props.children).forEach(child => {
-      // Create instance of children
-      let cc = new child.type();
-      this._updateCanvas({ name: cc.name, rough, ...child.props })
-    });
+      React.Children.toArray(this.props.children).forEach(child => {
+        const ChildComponent = new child.type();
+        this._updateRough({ name: ChildComponent.name, rough, ...child.props })
+      });
 
-  } else {
-    // No Child, Create a canvas without an id :p
-    canvas = document.createElement('canvas');
-    document.body.appendChild(canvas);
+    } else {
+      // No Child, Create a canvas without tranversing through each child
+      let rough = new RoughCanvas(this.refs.roughCanvas, width, height);
 
-    rough = new RoughCanvas(canvas, this.props.width, this.props.height);
-
-    this._updateCanvas({ name: this.name, rough, ...this.props });
-  }
-}
-
-_updateCanvas({ name, rough, onRender, options: { data, ...rest }}) {
-  let shape;
-
-  // Apply our supplied data to rough
-  // path => String, curve and polygon => Array
-  if (name.match(/curve|path|polygon/)) {
-    shape = rough[name](data);
-  } else {
-    shape = rough[name](...data);
+      // Update it ;)
+      this._updateRough({ name: this.name, rough, ...this.props });
+    }
   }
 
-  // Iterate over props and apply each prop to our element
-  Object.keys(rest).forEach(prop => shape[prop] = rest[prop]);
+  _updateRough({ name, rough, onRender, options: { data, ...rest }}) {
+    let shape;
 
-  // Apply our Hook
-  if (typeof onRender !== 'undefined') {
-    onRender(shape);
-  }
-}
+    // Apply our supplied data to rough
+    // path => String, curve and polygon => Array
+    if (name.match(/curve|path|polygon/)) {
+      shape = rough[name](data);
+    } else {
+      shape = rough[name](...data);
+    }
 
-  _deleteCanvas() {
-    document.body.removeChild(this.canvas);
+    // Iterate over props and apply each prop to our element
+    Object.keys(rest).forEach(prop => shape[prop] = rest[prop]);
+
+    //Apply our Hook
+    if (typeof onRender !== 'undefined') {
+      onRender(shape);
+    }
   }
 
   componentDidMount() {
     this._prepareRough();
   }
 
-  componentWillUnmount() {
-    this._deleteCanvas();
-  }
-
   render() {
-    return null;
+    return <canvas ref='roughCanvas'/>;
   }
 }
 
@@ -150,5 +117,6 @@ export {
   Line,
   Path,
   Polygon,
-  Rectangle
+  Rectangle,
+  ReactRough
 };
