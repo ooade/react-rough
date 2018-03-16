e902374017232696da2d5b834f1af62116ca88b2
Ademola Adegbuyi <ooade96@gmail.com>
2018-03-16 16:47:07 +0100

    Basic Impl with render props

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ReactRough, { Circle } from '../../src'
+import ReactRough from '../../src'
 
 const App = () => (
-	<div>
-		<Circle width="300" height="500" data={[50, 50, 80]} />
-		<Circle width="300" height="500" data={[50, 50, 80]} />
-	</div>
+	<ReactRough
+		width="500"
+		height="200"
+		render={rc => {
+			rc.circle(80, 120, 50) // centerX, centerY, diameter
+			rc.ellipse(300, 100, 150, 80) // centerX, centerY, width, height
+			rc.line(80, 120, 300, 100) // x1, y1, x2, y2
+		}}
+	>
+		{/* <Circle data={[80, 120, 50]} />
+		<Ellipse data={[300, 100, 150, 80]} />
+		<Line data={[80, 120, 300, 100]} /> */}
+	</ReactRough>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
--- src/index.js
+++ src/index.js
@@ -1,108 +1,28 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import Rough from 'roughjs'
 import PropTypes from 'prop-types'
-import createReactContext from 'create-react-context'
-
-const RoughContext = createReactContext('rough')
-
-class RoughProvider extends Component {
-	setCanvas = canvas => (this.canvas = canvas)
-
-	setData = data => (this.data = data)
-
-	setWidth = width => (this.width = width)
-
-	setHeight = height => (this.height = height)
-
-	setShape = shape => (this.shape = shape)
-
-	componentDidMount() {
-		console.log(this.canvas)
-		let rc = Rough.canvas(this.canvas)
-
-		switch (this.shape) {
-			case 'circle':
-				rc.circle(...this.data)
-				break
-		}
-	}
-
-	render() {
-		return (
-			<RoughContext.Provider
-				value={{
-					setCanvas: this.setCanvas,
-					setData: this.setData,
-					setWidth: this.setWidth,
-					setHeight: this.setHeight,
-					setShape: this.setShape
-				}}
-			>
-				{this.props.children}
-			</RoughContext.Provider>
-		)
-	}
-}
 
 class ReactRough extends Component {
+	componentDidMount() {
+		const { height, width, render } = this.props
+
+		let canvas = document.createElement('canvas')
+		canvas.height = height
+		canvas.width = width
+
+		let rc = Rough.canvas(canvas)
+
+		render(rc)
+
+		ReactDOM.findDOMNode(this).appendChild(canvas)
+	}
+
 	render() {
-		return (
-			<RoughProvider>
-				<RoughContext.Consumer>
-					{({ setCanvas, setData, setHeight, setWidth, setShape }) => {
-						let canvas = document.createElement('canvas')
-						canvas.id = 'rough'
+		const { height, width, render } = this.props
 
-						if (!document.getElementById('rough')) {
-							document.body.appendChild(canvas)
-						}
-
-						setWidth(500)
-						setHeight(200)
-						setCanvas(canvas)
-
-						return React.cloneElement(this.props.children, {
-							hasParent: true,
-							setData,
-							setShape
-						})
-					}}
-				</RoughContext.Consumer>
-			</RoughProvider>
-		)
+		return <div />
 	}
 }
 
-class Circle extends Component {
-	render() {
-		const { data, height, width, hasParent, setData, setShape } = this.props
-
-		if (!this.hasParent)
-			return (
-				<div>
-					<canvas ref={i => (this.canvas = i)} />
-					<RoughProvider>
-						<RoughContext.Consumer>
-							{({ setCanvas, setData, setHeight, setWidth, setShape }) => {
-								console.log(this.canvas)
-								setCanvas(this.canvas)
-								setData(data)
-								setHeight(height)
-								setWidth(width)
-								setShape('circle')
-
-								return null
-							}}
-						</RoughContext.Consumer>
-					</RoughProvider>
-				</div>
-			)
-
-		setData(this.data)
-		setShape(shape)
-		return null
-	}
-}
-
-export { Circle }
 export default ReactRough
