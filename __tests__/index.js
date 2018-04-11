ff666a18f4043710d2f6cad1ca06f0f838a7a4db
Ademola Adegbuyi <ooade96@gmail.com>
2018-04-11 23:17:56 +0100

    New API

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,17 +1,11 @@
 import React from 'react'
 import { mount } from 'enzyme'
 
-import Adapter from 'enzyme-adapter-react-16'
-import EnzymeToJSON from 'enzyme-to-json'
-
 import ReactRough from '../src'
 
-// Setup Enzyme Adapter
-configure({ adapter: new Adapter() })
-
-// describe('ReactRough', () => {
-// test('renders correctly', () => {
-// 	const component = mount(<ReactRough />)
-// 	expect(EnzymeToJSON(component)).toMatchSnapshot()
-// })
-// })
+describe('ReactRough', () => {
+	test('renders correctly', () => {
+		const component = mount(<ReactRough />)
+		expect(component).toMatchSnapshot()
+	})
+})
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Rough from 'roughjs'
 
 class ReactRough extends Component {
 	componentDidMount() {
-		const { height, width, render } = this.props
+		const { height, width, render, children } = this.props
 
 		let canvas = document.createElement('canvas')
 		canvas.height = height
@@ -12,7 +12,17 @@ class ReactRough extends Component {
 
 		let rc = Rough.canvas(canvas)
 
-		render(rc)
+		if (render) render(rc)
+
+		if (children) {
+			React.Children.map(children, child => {
+				const type = child.type.name.toLowerCase()
+
+				const { points, ...data } = child.props
+
+				rc[type](...points, data)
+			})
+		}
 
 		ReactDOM.findDOMNode(this).appendChild(canvas)
 	}
@@ -20,6 +30,102 @@ class ReactRough extends Component {
 	render() {
 		return <div />
 	}
+}
+
+export const Arc = ({ width, height, points, ...data }) => {
+	return (
+		<ReactRough
+			width={width}
+			height={height}
+			render={rc => {
+				rc.arc(...points, data)
+			}}
+		/>
+	)
+}
+
+export const Circle = ({ width, height, onRender, points, ...data }) => {
+	return (
+		<ReactRough
+			width={width}
+			height={height}
+			render={rc => {
+				rc.circle(...points, data)
+			}}
+		/>
+	)
+}
+
+export const Curve = ({ width, height, points, ...data }) => {
+	return (
+		<ReactRough
+			width={width}
+			height={height}
+			render={rc => {
+				rc.curve(...points, data)
+			}}
+		/>
+	)
+}
+
+export const Ellipse = ({ width, height, points, ...data }) => {
+	return (
+		<ReactRough
+			width={width}
+			height={height}
+			render={rc => {
+				rc.ellipse(...points, data)
+			}}
+		/>
+	)
+}
+
+export const Line = ({ width, height, points, ...data }) => {
+	return (
+		<ReactRough
+			width={width}
+			height={height}
+			render={rc => {
+				rc.line(...points, data)
+			}}
+		/>
+	)
+}
+
+export const Path = ({ width, height, points, ...data }) => {
+	return (
+		<ReactRough
+			width={width}
+			height={height}
+			render={rc => {
+				rc.path(...points, data)
+			}}
+		/>
+	)
+}
+
+export const Polygon = ({ width, height, points, ...data }) => {
+	return (
+		<ReactRough
+			width={width}
+			height={height}
+			render={rc => {
+				rc.polygon(...points, data)
+			}}
+		/>
+	)
+}
+
+export const Rectangle = ({ width, height, points, ...data }) => {
+	return (
+		<ReactRough
+			width={width}
+			height={height}
+			render={rc => {
+				rc.rectangle(...points, data)
+			}}
+		/>
+	)
 }
 
 export default ReactRough
