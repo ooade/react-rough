1dbcba20e66bfe133f0d69f479540633cf466c3c
Ashinze Ekene <ashinzekene@gmail.com>
2019-11-23 10:15:05 +0100

    Fix node update (#234)
    * Fix nodes issue
    
    * fix rough children component update issue

--- src/RoughComponents.tsx
+++ src/RoughComponents.tsx
@@ -6,11 +6,15 @@ import * as Props from './RoughComponentProps';
 
 export { Point } from 'roughjs/bin/geometry';
 
+const random = () => `${Math.round(Math.random() * 100000)}`.padEnd(7, '0');
+
 export const Line: FC<Props.LineProps> = ({ x1, y1, x2, y2, ...props }) => {
-	const { render } = useContext(RoughContext);
+	const { render, remove } = useContext(RoughContext);
+	const token = `line-${random()}`;
 
 	useDeepCompareEffect(() => {
-		render(rc => rc.line(x1, y1, x2, y2, props));
+		render(rc => [rc.line(x1, y1, x2, y2, props), token]);
+		return () => remove(token);
 	}, [x1, y1, x2, y2, props]);
 
 	return null;
@@ -24,10 +28,12 @@ export const Rectangle: FC<Props.RectangleProps> = ({
 	height,
 	...props
 }) => {
-	const { render } = useContext(RoughContext);
+	const { render, remove } = useContext(RoughContext);
+	const token = `rect-${random()}`;
 
 	useDeepCompareEffect(() => {
-		render(rc => rc.rectangle(x, y, width, height, props));
+		render(rc => [rc.rectangle(x, y, width, height, props), token]);
+		return () => remove(token);
 	}, [x, y, width, height, props]);
 
 	return null;
@@ -41,10 +47,12 @@ export const Ellipse: FC<Props.EllipseProps> = ({
 	height,
 	...props
 }) => {
-	const { render } = useContext(RoughContext);
+	const { render, remove } = useContext(RoughContext);
+	const token = `ellipse-${random()}`;
 
 	useDeepCompareEffect(() => {
-		render(rc => rc.ellipse(x, y, width, height, props));
+		render(rc => [rc.ellipse(x, y, width, height, props), token]);
+		return () => remove(token);
 	}, [x, y, width, height, props]);
 
 	return null;
@@ -52,10 +60,12 @@ export const Ellipse: FC<Props.EllipseProps> = ({
 Ellipse.displayName = 'Ellipse';
 
 export const Circle: FC<Props.CircleProps> = ({ x, y, diameter, ...props }) => {
-	const { render } = useContext(RoughContext);
+	const { render, remove } = useContext(RoughContext);
+	const token = `circle-${random()}`;
 
 	useDeepCompareEffect(() => {
-		render(rc => rc.circle(x, y, diameter, props));
+		render(rc => [rc.circle(x, y, diameter, props), token]);
+		return () => remove(token);
 	}, [x, y, diameter, props]);
 
 	return null;
@@ -63,10 +73,12 @@ export const Circle: FC<Props.CircleProps> = ({ x, y, diameter, ...props }) => {
 Circle.displayName = 'Circle';
 
 export const LinearPath: FC<Props.LinearPathProps> = ({ points, ...props }) => {
-	const { render } = useContext(RoughContext);
+	const { render, remove } = useContext(RoughContext);
+	const token = `linear-${random()}`;
 
 	useDeepCompareEffect(() => {
-		render(rc => rc.linearPath(points, props));
+		render(rc => [rc.linearPath(points, props), token]);
+		return () => remove(token);
 	}, [points, props]);
 
 	return null;
@@ -74,10 +86,12 @@ export const LinearPath: FC<Props.LinearPathProps> = ({ points, ...props }) => {
 LinearPath.displayName = 'LinearPath';
 
 export const Polygon: FC<Props.PolygonProps> = ({ points, ...props }) => {
-	const { render } = useContext(RoughContext);
+	const { render, remove } = useContext(RoughContext);
+	const token = `poly-${random()}`;
 
 	useDeepCompareEffect(() => {
-		render(rc => rc.polygon(points, props));
+		render(rc => [rc.polygon(points, props), token]);
+		return () => remove(token);
 	}, [points, props]);
 
 	return null;
@@ -94,10 +108,15 @@ export const Arc: FC<Props.ArcProps> = ({
 	closed,
 	...props
 }) => {
-	const { render } = useContext(RoughContext);
+	const { render, remove } = useContext(RoughContext);
+	const token = `arc-${random()}`;
 
 	useDeepCompareEffect(() => {
-		render(rc => rc.arc(x, y, width, height, start, stop, closed, props));
+		render(rc => [
+			rc.arc(x, y, width, height, start, stop, closed, props),
+			token
+		]);
+		return () => remove(token);
 	}, [x, y, width, height, start, stop, closed, props]);
 
 	return null;
@@ -105,10 +124,12 @@ export const Arc: FC<Props.ArcProps> = ({
 Arc.displayName = 'Arc';
 
 export const Curve: FC<Props.CurveProps> = ({ points, ...props }) => {
-	const { render } = useContext(RoughContext);
+	const { render, remove } = useContext(RoughContext);
+	const token = `curve-${random()}`;
 
 	useDeepCompareEffect(() => {
-		render(rc => rc.curve(points, props));
+		render(rc => [rc.curve(points, props), token]);
+		return () => remove(token);
 	}, [points, props]);
 
 	return null;
@@ -116,10 +137,12 @@ export const Curve: FC<Props.CurveProps> = ({ points, ...props }) => {
 Curve.displayName = 'Curve';
 
 export const Path: FC<Props.PathProps> = ({ d, ...props }) => {
-	const { render } = useContext(RoughContext);
+	const { render, remove } = useContext(RoughContext);
+	const token = `path-${random()}`;
 
 	useDeepCompareEffect(() => {
-		render(rc => rc.path(d, props));
+		render(rc => [rc.path(d, props), token]);
+		return () => remove(token);
 	}, [d, props]);
 
 	return null;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useEffect } from 'react';
+import useDeepCompareEffect from 'use-deep-compare-effect';
 import { RoughSVG } from 'roughjs/bin/svg';
 import { RoughCanvas } from 'roughjs/bin/canvas';
 import { Config, Drawable } from 'roughjs/bin/core';
@@ -10,14 +11,16 @@ interface RoughProps {
 	config?: Config;
 }
 
-type RoughCallback = (rc: RoughGenerator) => Drawable;
+type RoughCallback = (rc: RoughGenerator) => [Drawable, string];
 
 interface RoughContextProps {
 	render: (callback: RoughCallback) => void;
+	remove: (token: string) => void;
 }
 
 export const RoughContext = React.createContext<RoughContextProps>({
-	render: () => {}
+	render: () => {},
+	remove: () => {}
 });
 
 const ReactRoughCanvas: FC<RoughProps> = ({
@@ -26,41 +29,36 @@ const ReactRoughCanvas: FC<RoughProps> = ({
 	height = 150,
 	children
 }) => {
-	const ref = React.createRef<HTMLCanvasElement>();
-	const nextRoughCanvas = React.useRef<RoughCanvas>();
-	const node = React.useRef<Drawable>();
+	const canvasRef = React.createRef<HTMLCanvasElement>();
+	const roughCanvasRef = React.useRef<RoughCanvas>();
+	const nodes = React.useRef<{ [key: string]: Drawable }>({});
+
+	const remove = (token: string) => delete nodes.current[token];
 
 	const render = (callback: RoughCallback): void => {
-		const canvas = ref.current;
-		if (canvas) {
-			const roughCanvas = new RoughCanvas(canvas, config);
-			node.current = callback(roughCanvas.generator);
-			nextRoughCanvas.current = roughCanvas;
+		if (!canvasRef.current) return;
+		if (canvasRef.current && !roughCanvasRef.current) {
+			roughCanvasRef.current = new RoughCanvas(canvasRef.current, config);
 		}
+		const [node, token] = callback(roughCanvasRef.current!.generator);
+		nodes.current[token] = node;
 	};
 
-	useEffect(() => {
-		console.log(node);
-	});
-
-	useEffect(() => {
-		const roughCanvas = ref.current;
-
-		if (!nextRoughCanvas.current || !node.current || !roughCanvas) return;
-
-		nextRoughCanvas.current.draw(node.current);
-
-		return (): void => {
-			const ctx = roughCanvas.getContext('2d');
-			if (ctx) {
-				ctx.clearRect(0, 0, width, height);
-			}
+	useDeepCompareEffect(() => {
+		roughCanvasRef.current = new RoughCanvas(canvasRef.current!, config);
+		Object.values(nodes.current).forEach(node =>
+			roughCanvasRef.current!.draw(node)
+		);
+		const canvas = canvasRef.current;
+		return () => {
+			if (!canvas) return;
+			canvas.getContext('2d')!.clearRect(0, 0, width, height);
 		};
-	}, [height, width, ref]);
+	}, [height, width, canvasRef, config]);
 
 	return (
-		<RoughContext.Provider value={{ render }}>
-			<canvas width={width} height={height} ref={ref}>
+		<RoughContext.Provider value={{ render, remove }}>
+			<canvas width={width} height={height} ref={canvasRef}>
 				{children}
 			</canvas>
 		</RoughContext.Provider>
@@ -73,35 +71,38 @@ export const ReactRoughSvg: FC<RoughProps> = ({
 	height = 150,
 	children
 }) => {
-	const ref = React.createRef<SVGSVGElement>();
-	const nextRoughSvg = React.useRef<RoughSVG>();
-	const node = React.useRef<Drawable>();
+	const svgRef = React.createRef<SVGSVGElement>();
+	const roughSvgRef = React.useRef<RoughSVG>();
+	const nodes = React.useRef<{ [key: string]: Drawable }>({});
 
-	const render = (callback: RoughCallback): void => {
-		const svg = ref.current;
-		if (svg) {
-			const roughSvg = new RoughSVG(svg, config);
-			node.current = callback(roughSvg.generator);
-			nextRoughSvg.current = roughSvg;
-		}
+	const remove = (token: string) => {
+		delete nodes.current[token];
 	};
 
-	useEffect(() => {
-		const roughSvg = ref.current;
+	const render = (callback: RoughCallback): void => {
+		const svg = svgRef.current;
+		if (!svg) return;
+		if (svg && !roughSvgRef.current) {
+			roughSvgRef.current = new RoughSVG(svg, config);
+		}
+		const [node, token] = callback(roughSvgRef.current!.generator);
+		nodes.current[token] = node;
+	};
 
-		if (!nextRoughSvg.current || !node.current || !roughSvg) return;
-
-		const g = nextRoughSvg.current.draw(node.current);
-		roughSvg.appendChild(g);
-
-		return (): void => {
-			roughSvg.removeChild(g);
+	useDeepCompareEffect(() => {
+		const svg = svgRef.current;
+		if (!svg) return;
+		Object.values(nodes.current)
+			.map(s => roughSvgRef.current!.draw(s))
+			.map(svgEl => svg.appendChild(svgEl));
+		return () => {
+			svg.innerHTML = '';
 		};
-	}, [ref]);
+	}, [height, width, svgRef, config]);
 
 	return (
-		<RoughContext.Provider value={{ render }}>
-			<svg width={width} height={height} ref={ref}>
+		<RoughContext.Provider value={{ render, remove }}>
+			<svg width={width} height={height} ref={svgRef}>
 				{children}
 			</svg>
 		</RoughContext.Provider>
